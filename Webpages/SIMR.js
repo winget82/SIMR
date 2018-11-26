c656fa9c487f26e7dccdc19ef670b3798546cd51
winget82 <nflesher82@yahoo.com>
2018-11-26 14:34:20 -0600

    Updates 11/26/2018 14:34

--- Webpages/SIMR.js
+++ Webpages/SIMR.js
@@ -1,68 +1,66 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myDropdownBook() {
-    document.getElementById("myDropdownBook").classList.toggle("show");
-}
-
-function myDropdownChapter() {
-    document.getElementById("myDropdownChapter").classList.toggle("show");
-}
-
-function myDropdownVerse() {
-    document.getElementById("myDropdownVerse").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');}
-        }
-    }
-}
-
-//PROBLEMS WITH THESE FUNCTIONS THEY ALL CHANGE THE SAME BUTTON REGARDLESS OF DIFFERENT VARIABLE AND ID NAMES
-
-function button_clickBook() {
-    var buttons = document.getElementsByTagName("a");
+/*FUNCTIONS TO RETURN THE VALUE OF SELECTION FROM SELECTIONS DROPDOWNS*/
+function selectedChoiceBook() {
+    var buttons = document.getElementsByTagName("option");
     var buttonsCount = buttons.length;
     var textReturnedBook;
     for (var i = 0; i < buttonsCount; i += 1) {
         buttons[i].onclick = function(e) {
-            //alert(this.innerText);
+            alert(this.innerText);
             textReturnedBook = this.innerText;
-            document.getElementById("book").innerHTML = textReturnedBook;
+            return textReturnedBook;
+            //return this.value;
         };
     }
 }
 
-function button_clickChapter() {
-    var buttons = document.getElementsByTagName("a");
+function selectedChoiceChapter() {
+    var buttons = document.getElementsByTagName("option");
     var buttonsCount = buttons.length;
     var textReturnedChapter;
     for (var i = 0; i < buttonsCount; i += 1) {
         buttons[i].onclick = function(e) {
-            //alert(this.innerText);
+            alert(this.innerText);
             textReturnedChapter = this.innerText;
-            document.getElementById("chapter").innerHTML = textReturnedChapter;
+            return textReturnedChapter;
+            //return this.value;
         };
     }
 }
 
-function button_clickVerse() {
-    var buttons = document.getElementsByTagName("a");
+function selectedChoiceVerse() {
+    var buttons = document.getElementsByTagName("option");
     var buttonsCount = buttons.length;
     var textReturnedVerse;
     for (var i = 0; i < buttonsCount; i += 1) {
         buttons[i].onclick = function(e) {
-            //alert(this.innerText);
+            alert(this.innerText);
             textReturnedVerse = this.innerText;
-            document.getElementById("verse").innerHTML = textReturnedVerse;
+            return textReturnedVerse;
+            //return this.value;
         };
     }
+}
+
+var Book = selectedChoiceBook();
+var Chapter = selectedChoiceChapter();
+var Verse = selectedChoiceVerse();
+
+/*FUNCTION TO RETURN A STRING OF COMPILED RETURNS FROM SELECTION DROPDOWNS TO BE USED
+AS STRING TO SEARCH JSON FILES*/
+function searchChoice() {
+    var book = document.getElementById("BookSelect");
+    var i = book.selectedIndex;
+    var Book = book.options[i].text;
+    
+    var chapter = document.getElementById("ChapterSelect");
+    var i = chapter.selectedIndex;
+    var Chapter = chapter.options[i].text;
+
+    var verse = document.getElementById("VerseSelect");
+    var i = verse.selectedIndex;
+    var Verse = verse.options[i].text;
+    
+    var searchString = Book + " " + Chapter + Verse;
+    alert(searchString);
+    return searchString;
 }
