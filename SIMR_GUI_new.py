eb03ed52f63486430556b6bd1b1f2d2b64d74d50
winget82 <nflesher82@yahoo.com>
2019-04-05 23:38:30 -0500

    Updates

--- SIMR_GUI_new.py
+++ SIMR_GUI_new.py
@@ -1,12 +1,155 @@
+# -*- coding: utf-8 -*-
+
+#Program written by N. Flesher
+
+"""Program for study of the scriptures"""
+
+# ---------------------------------------------------------------------
+# IMPORTS - PACKAGES & MODULES UTILIZED
+# ---------------------------------------------------------------------
 import sys
 from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
 from PyQt5.QtGui import QIcon
+import re
+import codecs
+import json
+import ctypes
+import time
+
+
+# ---------------------------------------------------------------------
+# OPENING OF FILES & GENERATING OF DICTIONARIES AND LISTS
+# ---------------------------------------------------------------------
+
+fpath = './json_files/'
+
+# Berean Bibles
+Berean_file = 'berean_json.json'
+with open(fpath + Berean_file) as bereanfile:
+    berean = json.load(bereanfile)
+    
+# KJV
+KJV_file = 'KJV_json.json'
+with open(fpath + KJV_file) as filekjv:
+    scriptures_lst = json.load(filekjv)
+
+books = 'KJVbooks_json.json'
+with open(fpath + books) as filebk:
+    alph_books = json.load(filebk)
+
+# KJV WITH STRONGS
+folderpath = './json_files/StrongsNumbers/'
+
+OT_file = 'OT_json.json'  # OLD TESTAMENT
+with open(folderpath + OT_file) as f_obj_OT:
+    OT_sn = json.load(f_obj_OT)
+
+NT_file = 'NT_json.json'  # NEW TESTAMENT
+with open(folderpath + NT_file) as f_obj_NT:
+    NT_sn = json.load(f_obj_NT)
+
+# HowToEnjoyReadingTheBible Bullinger
+with open('./ref_files/how_to_enjoy_the_bible_bullinger.txt', 'r', encoding='ISO-8859-1') as fileenjoy:
+    enjoy = fileenjoy.read()#NEED TO SWAP THIS OUT WITH JSON FILE
+
+# NumbersInScripture Bullinger
+with open('./ref_files/number_in_scripture_bullinger.txt', 'r', encoding='ISO-8859-1') as filenumberscript:
+    numberscript = filenumberscript.read()#NEED TO SWAP THIS OUT WITH JSON FILE
+
+# WitnessOfTheStars Bullinger
+with open('./ref_files/thewitnessofthestars.txt', 'r', encoding='ISO-8859-1') as witnessstars:
+    stars = witnessstars.read()#NEED TO SWAP THIS OUT WITH JSON FILE
+
+# TWI scripture index
+with open('./ref_files/modified_for_python_SCRIPTURE_INDEX.txt', 'r', encoding='ISO-8859-1') as filetwi:
+    twi = filetwi.read()#NEED TO SWAP THIS OUT WITH JSON FILE
+
+# TWI scripture index abbreviations
+with open('./ref_files/modified_for_python_SCRIPTURE_INDEX_abbreviations.txt', 'r', encoding='ISO-8859-1') as filetwiabb:
+    twiabb = filetwiabb.read()#NEED TO SWAP THIS OUT WITH JSON FILE
+
+# Strongs Concordance Dictionary
+scd = "strongsconc_json.json"
+with open(fpath + scd) as concfile:
+    strongscsvlst = json.load(concfile)
+
+# TWI Scripture index split list by verses [verse reference, books & pages & etc.]:
+twi_index = [verse.split('\t') for verse
+             in twi.split('$') if verse.split('\t')]
+
+# Septuagint
+sept = 'septuagint_json.json'
+with open(fpath + sept) as sept_file2:
+    septuagint_lst3 = json.load(sept_file2)
+
+def sequenceOfFunctions(*functions):
+    def func(*args, **kwargs):
+        return_value = None
+        for function in functions:
+            return_value = function(*args, **kwargs)
+        return return_value
+    return func
+
+
+# ---------------------------------------------------------------------
+# SIMR Class
+# ---------------------------------------------------------------------
 
 class SIMR(QMainWindow):
     
     def __init__(self):
         super().__init__()
         self.program()
+        #Text for Pop Ups
+        self.about = """\n\n
+        About this program:
+        --------------------------\n
+        This program is called SIMR, which is an abbreviation
+        for "Scripture Indices & Ministry Resources."\n
+        This program was put together with the python programming
+        language, and is a compilation of the works of many, many
+        others.  Others who have spent countless hours studying &
+        teaching the Word of God.  My thanks to them for making
+        researching God's Word so much easier for us!\n
+        The goal of this program, is to provide a tool to easily
+        compile resources for researching God's Word.  A tool to
+        help redeem the time as we diligently study God's Word.
+        To 'simmer' is to be at a temperature just below the
+        boiling point.  It's to be in a state of the initial
+        stages of development...\n
+        I thought this fitting, as when we're researching a topic,
+        the research we are doing is developing into something more.
+        Such as a teaching or a research work.  Not only that, but
+        if you desire to boil over with God's Word, to heat to that
+        point, you've got to let the Word burn within you.  You
+        need to steep in it.  My believing is that this tool will
+        help you in your endeavours as you stand for God in this
+        day, time, and hour.\n
+        Love in Christ,
+        N. A. Flesher - 06/03/2018\n"""
+
+        self.credits = """\n\n
+        Credits, URL Links, & Resources Used:
+        ------------------------------------------------\n
+        Strong's Numbers & Definitions - viz.bible\n
+        The Holy Bible, Berean Study Bible, BSB
+              Copyright ©2016 by Bible Hub
+              Used by Permission. All Rights Reserved Worldwide.
+              http://berean.bible - Berean Bible Homepage\n
+        Data for maps and coordinate locations
+        are from OpenBible.info licensed under a
+        Creative Commons Attribution license.\n
+        The King James Version, Septuagint, and Strong's
+        Concordance, as well Ethelbert W. Bullinger's
+        books utilized in this program: The Witness of the Stars,
+        Number in Scripture, and How to Enjoy the Bible,
+        are all in the public domain.\n
+        Thanks to various Christian believers from different
+        walks of life, for their typing of the various works,
+        for their help in gathering resources, and their advice.\n
+        And thanks to YOU, for studying and speaking God's Word!
+        YOU ARE GOD'S BEST!!!
+        \n\n"""
         
     def program(self):
 
@@ -19,7 +162,7 @@ class SIMR(QMainWindow):
         saveProject.setStatusTip('Save your current project')
         #saveApp.triggered.connect()
 
-        closeApp = QAction(QIcon('exit.png'), '&Exit', self)        
+        closeApp = QAction('&Exit', self)        
         closeApp.setStatusTip('Close and exit SIMR')
         closeApp.triggered.connect(qApp.quit)
 
@@ -62,8 +205,14 @@ class SIMR(QMainWindow):
         drawMap.setStatusTip('Draw Bible map')
 
         # Help Menu Options
-        about = QAction('About', self)
-        about.setStatusTip('See information about this application')
+        aboutApp = QAction('About', self)
+        aboutApp.setStatusTip('See information about this application')
+
+        appDocumentation = QAction('Documentation', self)
+        appDocumentation.setStatusTip('See documentation about using this application')
+
+        creditsThanks = QAction('Credits / Thanks', self)
+        creditsThanks.setStatusTip('See credits and special thanks')
 
         self.statusBar()
 
@@ -103,18 +252,29 @@ class SIMR(QMainWindow):
 
         # Help Menu
         helpMenu = menubar.addMenu('&Help')
-        helpMenu.addAction(about)
+        helpMenu.addAction(appDocumentation)
+        helpMenu.addAction(creditsThanks)
+        helpMenu.addAction(aboutApp)
         
+        # Toolbar
+        #self.toolbar = self.addToolBar()
+        #self.toolbar.addAction()
+        # SEE - http://zetcode.com/gui/pyqt5/menustoolbars/
 
         self.setGeometry(600, 600, 600, 400)
         self.setWindowTitle('SIMR - Scripture Indices and Ministry Resources')    
         self.show()
     
+
     def buttonClick(self):
         alert = QMessageBox()
         alert.setText('Button has been clicked')
         alert.exec_()
-        
+
+    
+# ---------------------------------------------------------------------
+# Run App
+# ---------------------------------------------------------------------
         
 if __name__ == '__main__':
     
